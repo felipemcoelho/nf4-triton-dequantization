50a33c62dc501417aaccdfddbf92242ee74ec2d4
Felipe Coelho <felipemc@live.com>
2025-08-27 11:24:34 -0300

    Optimized NF4 dequantization using Triton

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nf4-triton-dequantization",
-    version="0.1.0",
+    version="0.2.0",
     description="Optimized Triton kernel for dequantizing NF4 tensors",
     author="Felipe Coelho",
     author_email="felipemc@live.com",
